193a322fb62e149018ec53a3b72b3821230b0032
Jürgen Hahn <juergen.hahn@ur.de>
2021-10-06 10:38:23 +0200

    add starter

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import de.ur.mi.oop.app.GraphicsApp;
+import de.ur.mi.oop.launcher.GraphicsAppLauncher;
+
+public class Main extends GraphicsApp {
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void draw() {
+    }
+
+    public static void main(String[] args) {
+        GraphicsAppLauncher.launch();
+    }
+}
